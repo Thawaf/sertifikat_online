61428c1e4b7813013d8bc4e5366583643106c62a
ammar <thawafammar@gmail.com>
2024-04-17 22:47:40 +0700

    index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <body>
     <h1>Selamat datang di Sertifikat Online</h1>
     <h2>Silakan login sebagai:</h2>
+    <h2>folder by ammar</h2>
     <ul>
         <li><a href="login_admin.php">Admin</a></li>
         <li><a href="login_user.php">User</a></li>
